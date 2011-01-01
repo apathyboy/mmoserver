bc80557862215d77bd0ac57675a1d559fe5d1657
Eric Barr <eric.barr@anhstudios.com>
2010-12-31 19:30:41 -0500

    Replaced DatabaseManager and added an initial set of tests for coverage of its features

--- src/anh/database/database_manager.cc
+++ src/anh/database/database_manager.cc
@@ -1,57 +1,133 @@
 /*
----------------------------------------------------------------------------------------
-This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+ This file is part of MMOServer. For more information, visit http://swganh.com
+ 
+ Copyright (c) 2006 - 2010 The SWG:ANH Team
 
-For more information, visit http://www.swganh.com
+ MMOServer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-Copyright (c) 2006 - 2010 The SWG:ANH Team
----------------------------------------------------------------------------------------
-Use of this source code is governed by the GPL v3 license that can be found
-in the COPYING file or at http://www.gnu.org/licenses/gpl-3.0.html
+ MMOServer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
----------------------------------------------------------------------------------------
+ You should have received a copy of the GNU General Public License
+ along with MMOServer.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+#define NOMINMAX
 
 #include "anh/database/database_manager.h"
 
-#include <algorithm>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
-#include "anh/database/database.h"
+#include <cppconn/driver.h>
+#include <cppconn/connection.h>
+#include <cppconn/prepared_statement.h>
 
+#include "anh/memory.h"
 
-void DatabaseManager::process() {
-    std::for_each(database_list_.begin(), database_list_.end(), 
-        [] (std::shared_ptr<Database> db) {
-            db->process();
-        });
+using namespace anh::database;
+using namespace boost::posix_time;
+using namespace std;
+
+DatabaseManager::DatabaseManager(sql::Driver* driver)   
+: driver_(driver)
+{}
+
+DatabaseManager::~DatabaseManager() {
+    std::for_each(connections_.begin(), connections_.end(), [] (ConnectionPoolMap::value_type& conn) {
+        if (conn.second.empty()) {
+            return;
+        }
+
+        do {
+            // close each connection and pop it from the list
+            auto connection = conn.second.front();
+            connection->close();
+            conn.second.pop();
+        } while (!conn.second.empty());
+    });
 }
 
 
-Database* DatabaseManager::connect(DBType type,
-                                   const std::string& host,
-                                   uint16_t port,
-                                   const std::string& user,
-                                   const std::string& pass,
-                                   const std::string& schema)
-{
-    // Create our new Database object and initiailzie it.
-	auto database = std::make_shared<Database>(this, type, host, port, user, pass, schema, database_configuration_);
+bool DatabaseManager::hasStorageType(StorageType storage_type) const {
+    ConnectionDataMap::const_accessor a;
+    return connection_data_.find(a, storage_type);
+}
 
-    // Add the new DB to our process list.
-    database_list_.push_back(database);
+bool DatabaseManager::registerStorageType(StorageType storage_type, const std::string& schema, const std::string& host, const std::string& username, const std::string& password) {
+    if (hasStorageType(storage_type)) {
+        return false;
+    }
 
-    return database.get();
+    // create a valid connection to verify the integrity of the data passed in
+    auto connection = std::shared_ptr<sql::Connection>(driver_->connect(host, username, password), 
+        std::bind(&DatabaseManager::recycleConnection_, this, storage_type, std::placeholders::_1));
+    connection->setSchema(schema);
+
+    // insert the data
+    ConnectionDataMap::accessor data_accessor;    
+    connection_data_.insert(data_accessor, storage_type);
+    data_accessor->second = make_shared<ConnectionData>(schema, host, username, password);
+
+    // add the created connection to the connection pool
+    ConnectionPoolMap::accessor pool_accessor;
+    connections_.insert(pool_accessor, storage_type);
+    pool_accessor->second.push(connection);
+
+    return true;
+}
+
+bool DatabaseManager::hasConnection(StorageType storage_type) const {
+    ConnectionPoolMap::const_accessor a;
+
+    // return false if unable to find a connection pool for the requested type
+    if (!connections_.find(a, storage_type)) {
+        return false;
+    }
+
+    // return whether or not the connection pool for this storage type is empty
+    return !a->second.empty();
+}
+
+shared_ptr<sql::Connection> DatabaseManager::requestConnection(StorageType storage_type) {
+    ConnectionPoolMap::accessor a;
+
+    // return false if unable to find a connection pool for the requested type
+    if (!connections_.find(a, storage_type) || a->second.empty()) {
+        // lookup the connection data for the requested storage type and fail out
+        // if not found
+        ConnectionDataMap::accessor data_accessor;
+        if (!connection_data_.find(data_accessor, storage_type)) {
+            assert(false && "Requested a storage type that has not been registered");
+            return nullptr;
+        }
+        
+        // create a valid connection to verify the integrity of the data passed in
+        auto connection_data = data_accessor->second;
+        auto connection = std::shared_ptr<sql::Connection>(driver_->connect(connection_data->host, connection_data->username, connection_data->password), 
+            std::bind(&DatabaseManager::recycleConnection_, this, storage_type, std::placeholders::_1));
+        connection->setSchema(connection_data->schema);
+
+        return connection;
+    }
+    
+    auto connection = a->second.front();
+    a->second.pop();
+
+    return connection;
+}
+
+void DatabaseManager::recycleConnection_(StorageType storage_type, sql::Connection* connection) {
+    if (connection->isClosed()) {
+        delete connection;
+        return;
+    }
+    
+    ConnectionPoolMap::accessor pool_accessor;
+    connections_.insert(pool_accessor, storage_type);
+    pool_accessor->second.push(std::shared_ptr<sql::Connection>(connection, std::bind(&DatabaseManager::recycleConnection_, this, storage_type, std::placeholders::_1)));
 }
--- src/anh/database/database_manager.h
+++ src/anh/database/database_manager.h
@@ -1,49 +1,65 @@
 /*
----------------------------------------------------------------------------------------
-This source file is part of SWG:ANH (Star Wars Galaxies - A New Hope - Server Emulator)
+ This file is part of MMOServer. For more information, visit http://swganh.com
+ 
+ Copyright (c) 2006 - 2010 The SWG:ANH Team
 
-For more information, visit http://www.swganh.com
+ MMOServer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
-Copyright (c) 2006 - 2010 The SWG:ANH Team
----------------------------------------------------------------------------------------
-Use of this source code is governed by the GPL v3 license that can be found
-in the COPYING file or at http://www.gnu.org/licenses/gpl-3.0.html
+ MMOServer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
----------------------------------------------------------------------------------------
+ You should have received a copy of the GNU General Public License
+ along with MMOServer.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef DATABASE_MANAGER_DATABASE_MANAGER_H_
 #define DATABASE_MANAGER_DATABASE_MANAGER_H_
 
 #include <cstdint>
-#include <list>
 #include <memory>
+#include <queue>
+#include <string>
 
 #include <boost/noncopyable.hpp>
 
+#include <tbb/concurrent_hash_map.h>
 #include <tbb/concurrent_queue.h>
 
-#include "anh/database/database_config.h"
-#include "anh/database/database_type.h"
+#include "anh/hash_string.h"
 
-struct DatabaseJob;
-class Database;
-class DatabaseWorkerThread;
+namespace sql {
+    class Connection;
+    class Driver;
+}
 
-/*! Manages multiple database connections.
+namespace anh {
+namespace database {
+    
+struct ConnectionData {
+    ConnectionData(std::string schema_, std::string host_, std::string username_, std::string password_)
+        : schema(std::move(schema_))
+        , host(std::move(host_))
+        , username(std::move(username_))
+        , password(std::move(password_))
+    {}
+
+    std::string schema;
+    std::string host;
+    std::string username;
+    std::string password;
+};
+
+typedef anh::HashString StorageType;
+typedef tbb::concurrent_hash_map<StorageType, std::shared_ptr<ConnectionData>> ConnectionDataMap;
+typedef std::queue<std::shared_ptr<sql::Connection>> ConnectionPool;
+typedef tbb::concurrent_hash_map<StorageType, ConnectionPool> ConnectionPoolMap;
+
+/*! Manages multithreaded database query processing.
 */
 class DatabaseManager : private boost::noncopyable {
 public:
@@ -53,44 +69,30 @@ public:
 	 * \param db_config Database configuration options.
 	 * \see DatabaseConfig
 	 */
-	explicit DatabaseManager(const DatabaseConfig& database_configuration)
-		: database_configuration_(database_configuration) { }
+	explicit DatabaseManager(sql::Driver* driver);
 
-    /*! Processes all current database connections.
-    */
-    void process();
+    ~DatabaseManager();
 
-    /*! Connects to a specified database.
-    *
-    * \param host The database host to connect to.
-    * \param port The port of the database host to connect to.
-    * \param user The username for accessing the requested schema.
-    * \param pass The password for accessing the requested schema.
-    * \param schema The database to connect to.
-    *
-    * \return The instance of the database created after successful connection.
-    */
-    Database* connect(DBType type, 
-        const std::string& host, 
-        uint16_t port, 
-        const std::string& user, 
-        const std::string& pass, 
-        const std::string& dbname);
+    bool hasStorageType(StorageType storage_type) const;
 
+    bool registerStorageType(StorageType storage_type, const std::string& schema, const std::string& host, const std::string& username, const std::string& password);
+
+    bool hasConnection(StorageType storage_type) const;
+
+    std::shared_ptr<sql::Connection> requestConnection(StorageType storage_type);
     
-
 private:
-    typedef std::list<std::shared_ptr<Database>> DatabaseList;
-    DatabaseList database_list_;
+    DatabaseManager();
 
-	DatabaseConfig database_configuration_;
+    void recycleConnection_(StorageType storage_type, sql::Connection* connection);
+
+    sql::Driver* driver_;
     
-    typedef tbb::concurrent_queue<DatabaseJob*> DatabaseJobQueue;
-    typedef tbb::concurrent_queue<DatabaseWorkerThread*> DatabaseWorkerThreadQueue;
-
-    DatabaseJobQueue job_pending_queue_;
-    DatabaseJobQueue job_complete_queue_;
-    DatabaseWorkerThreadQueue idle_worker_queue_;
+    ConnectionDataMap connection_data_;
+    ConnectionPoolMap connections_;
 };
+
+}  // namespace database
+}  // namespace anh
 
 #endif  // DATABASE_MANAGER_DATABASE_MANAGER_H_
