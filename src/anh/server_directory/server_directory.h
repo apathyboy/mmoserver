4222b40ab334ad0667fe0574935b87d40fa397dd
Eric Barr <eric.barr@anhstudios.com>
2011-01-02 08:26:06 -0500

    Added server_directory to the libanh project with an initial source and test file.

---
+++ src/anh/server_directory/server_directory.h
@@ -0,0 +1,34 @@
+/*
+ This file is part of MMOServer. For more information, visit http://swganh.com
+ 
+ Copyright (c) 2006 - 2010 The SWG:ANH Team
+
+ MMOServer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ MMOServer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with MMOServer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef ANH_SERVER_DIRECTORY_SERVER_DIRECTORY_H_
+#define ANH_SERVER_DIRECTORY_SERVER_DIRECTORY_H_
+
+namespace anh {
+namespace server_directory {
+
+/*! \brief ServerDirectory is a utility class intended to assist processes in
+* registering themselves and participating in a clustered environment.
+*/
+class ServerDirectory {};
+
+}  // namespace server_directory
+}  // namespace anh
+
+#endif  // ANH_SERVER_DIRECTORY_SERVER_DIRECTORY_H_
