5c18c91076fa13eec9063b332bea718b7f8f31ba
Eric Barr <eric.barr@anhstudios.com>
2011-01-06 12:19:50 -0500

    Moved implementation of functions to a cc file

--- src/anh/memory.h
+++ src/anh/memory.h
@@ -22,42 +22,20 @@
 
 #include "tbb/scalable_allocator.h"
 
-void* operator new (size_t size) {
-    if (size == 0) size = 1;
-    if (void* ptr = scalable_malloc(size))
-        return ptr;
-    throw std::bad_alloc();
-}
+void* operator new (size_t size);
 
-void* operator new[] (size_t size) {
-    return operator new (size);
-}
+void* operator new[] (size_t size);
 
-void* operator new (size_t size, const std::nothrow_t&) throw () {
-    if (size == 0) size = 1;
-    if (void* ptr = scalable_malloc(size))
-        return ptr;
-    return nullptr;
-}
+void* operator new (size_t size, const std::nothrow_t&) throw ();
 
-void* operator new[ ] (size_t size, const std::nothrow_t&) throw () {
-    return operator new (size, std::nothrow);
-}
+void* operator new[ ] (size_t size, const std::nothrow_t&) throw ();
 
-void operator delete (void* ptr) throw () {
-    if (ptr != 0) scalable_free(ptr);
-}
+void operator delete (void* ptr) throw ();
 
-void operator delete[] (void* ptr) throw () {
-    operator delete (ptr);
-}
+void operator delete[] (void* ptr) throw ();
 
-void operator delete (void* ptr, const std::nothrow_t&) throw () {
-    if (ptr != 0) scalable_free(ptr);
-}
+void operator delete (void* ptr, const std::nothrow_t&) throw ();
 
-void operator delete[] (void* ptr, const std::nothrow_t&) throw () {
-    operator delete (ptr, std::nothrow);
-}
+void operator delete[] (void* ptr, const std::nothrow_t&) throw ();
 
 #endif  // ANH_MEMORY_H_
