96d60ef87b46111f8109bd15b716fa4be2ea3b93
Eric Barr <eric.barr@anhstudios.com>
2011-01-11 19:16:23 -0500

    Added an example project to show how the new unit tests and nested directory functionality is working.

---
+++ src/login_service/login_service.h
@@ -0,0 +1,41 @@
+/*
+ This file is part of MMOServer. For more information, visit http://swganh.com
+ 
+ Copyright (c) 2006 - 2010 The SWG:ANH Team
+
+ MMOServer is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ MMOServer is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with MMOServer.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef LOGIN_SERVICE_LOGIN_SERVICE_H_
+#define LOGIN_SERVICE_LOGIN_SERVICE_H_
+
+#include "anh/application.h"
+
+namespace login_service {
+
+class LoginService : public anh::BaseApplication {
+public:
+    ~LoginService();
+    
+    bool hasStarted() const;
+
+private:    
+    void onStartup();
+    void onProcess();
+    void onShutdown();
+};
+
+}  // namespace login_service
+
+#endif  // LOGIN_SERVICE_LOGIN_SERVICE_H_
